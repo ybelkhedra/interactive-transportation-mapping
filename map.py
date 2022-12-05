5c277632ae2d7e8ea83a1cead7e1b02990b027c6
FICHET Felicien <Felicien.Fichet@bordeaux-inp.fr>
2022-12-05 19:57:21 +0100

    new map

--- map.py
+++ map.py
@@ -1,13 +1,51 @@
-import folium
-import requests
-import json
-map = folium.Map(location=[44.795170, -0.603537], zoom_start=14)
+import folium ## folium est une bibliothèque qui permet de créer des cartes interactives en s'appuyant sur leaflet.js
+import requests ## requests est une bibliothèque qui permet de faire des requêtes http pour les API
+import json ## json est une bibliothèque qui permet de manipuler des fichiers json
+import time
 
-with open('parking_cord.json') as mon_fichier:
+map = folium.Map(location=[44.795170, -0.603537], zoom_start=14) ## On crée une carte centrée sur Bordeaux
+
+
+
+parking_group = folium.FeatureGroup(name="Parking").add_to(map) ## On crée un groupe pour les parkings
+with open('parking_cord.json') as mon_fichier: ## On ouvre le fichier json qui contient les coordonnées des parkings
+    data = json.load(mon_fichier) ## On charge le fichier json dans la variable data
+    for i in data:
+        parking_group.add_child(folium.Marker(location=[i["geometry"]["coordinates"][1], i["geometry"]["coordinates"][0]], popup="C'est un parking !", icon=folium.Icon(color='red'))) ## On ajoute un marqueur pour chaque parking        
+
+
+pdc_group = folium.FeatureGroup(name="Points de charge").add_to(map) 
+with open('point_de_charge_cord.json') as mon_fichier:
     data = json.load(mon_fichier)
     for i in data:
-        map.add_child(folium.Marker(location=[i["geometry"]["coordinates"][1], i["geometry"]["coordinates"][0]], popup="C'est un parking !", icon=folium.Icon(color='red')))
-        
-        
-map.save(outfile='map.html')
+        pdc_group.add_child(folium.Marker(location=[i["geometry"]["coordinates"][1], i["geometry"]["coordinates"][0]], popup="C'est un point de charge !", icon=folium.Icon(color='green')))
+
+covoiturage_group = folium.FeatureGroup(name="Covoiturage").add_to(map)
+with open('covoiturage_cord.json') as mon_fichier:
+    data = json.load(mon_fichier)
+    for i in data:
+        covoiturage_group.add_child(folium.Marker(location=[i["geometry"]["coordinates"][1], i["geometry"]["coordinates"][0]], popup="C'est un point de covoiturage !", icon=folium.Icon(color='blue')))
+
+
+autopartage_group = folium.FeatureGroup(name="Autopartage").add_to(map)
+with open('autopartage_cord.json') as mon_fichier:
+    data = json.load(mon_fichier)
+    for i in data:
+        autopartage_group.add_child(folium.Marker(location=[i["geometry"]["coordinates"][1], i["geometry"]["coordinates"][0]], popup="C'est un point d'autopartage !", icon=folium.Icon(color='orange')))
+
+dico_chemins = {}
+lignes_group = folium.FeatureGroup(name="Lignes de transport").add_to(map)
+
+with open('chemins_lignes.json') as mon_fichier:
+    data = json.load(mon_fichier)
+    for i in data:
+        tmp = i['fields']['geo_shape']['coordinates']
+        # inverser les coordonnées dans tmp
+        for j in tmp:
+            j.reverse()
+        folium.PolyLine(i['fields']['geo_shape']['coordinates'], color="red", weight=2.5, opacity=1).add_to(lignes_group)
+
+    
+folium.LayerControl().add_to(map) ## On ajoute un contrôle pour activer/désactiver les couche
+map.save(outfile='map.html') ## On sauvegarde la carte dans un fichier html
 
