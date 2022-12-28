5426ec13a0509c529ccaec9c06091b41080d4942
Alexandre Soulié <alexandre.soulie@bordeaux-inp.fr>
2022-12-28 17:45:28 +0100

    Code commenté

--- htmlToJS.py
+++ htmlToJS.py
@@ -5,8 +5,8 @@ htmlEndTag = "</html>"
 layerControlVar = "var layer_control"
 mapVar = "var map"
 
-onWrite = False
-writeNewHTML = False
+onWrite = False # Initialise la variable qui décide si la balise <script> à été repérée et si le programme doit générer le JS, cette variable doit rester False
+writeNewHTML = False # Décide si l'on veut générer un nouveau HTML ou non
 
 with open("map.html","r") as htmlFile:
     with open("map.js","w+") as jsFile:
@@ -14,22 +14,24 @@ with open("map.html","r") as htmlFile:
             with open("mapHelper.js","w+") as helperFile:
                 while True:
                     line = htmlFile.readline()
-                    if not line:
+                    if not line: # Si la ligne est vide (null), arrêter la lecture
                         break
-                    if scriptTag in line:
+
+                    if scriptTag in line: # Si la balise <script> est détectée, écrire le JS
                         onWrite = True
-                    elif scriptEndTag in line:
+                    elif scriptEndTag in line: # Si la balise </script> est détectée, ne plus écrire le JS
                         onWrite = False
-                        if scriptSrcTag in line and writeNewHTML:
+                        if scriptSrcTag in line and writeNewHTML: # Si la balise "<script src=" est détectée et que l'écriture d'un nouveau HTML est activée, écrire la ligne
                             newHtmlFile.write(line)
-                    elif onWrite:
+                    elif onWrite: #Ecrit le JS
                         jsFile.write(line)
                         jsFile.write("\n")
-                    elif writeNewHTML:
-                        if htmlEndTag in line:
+                    elif writeNewHTML: #Ecrit le HTML
+                        if htmlEndTag in line: #Ajoute le nouveau JS au nouveau HTML
                             newHtmlFile.write('<script src="map.js"></script>\n')
                         newHtmlFile.write(line)
-                    if layerControlVar in line:
+
+                    if layerControlVar in line: #Trouve la variable de layer_control et l'assigne a une autre variable plus simple d'utilisation
                         i=0
                         while line[i] == " ":
                             i+=1
@@ -41,7 +43,8 @@ with open("map.html","r") as htmlFile:
                             helperFile.write(line[i])
                             i+=1
                         helperFile.write(";\n")
-                    if mapVar in line:
+
+                    if mapVar in line: #Trouve la variable de map et l'assigne a une autre variable plus simple d'utilisation
                         i=0
                         while line[i] == " ":
                             i+=1
