3f3f0b15e85d36751e2ba04bfa13d0c515e1d6cc
FICHET Felicien <Felicien.Fichet@bordeaux-inp.fr>
2023-01-17 06:18:58 +0100

    correction

--- sources/addCoordinatesLine.js
+++ sources/addCoordinatesLine.js
@@ -2,8 +2,8 @@ function addCoordinatesLine(nbCoords) {
     var formLines = document.getElementById("add-coordinates");
     var newLine = document.createElement("div");
     newLine.classList.add("form-line");
-    newLine.innerHTML = "<label for='latitude'>Latitude</label>" + "<input type='text' name='latitude' id='latitude" + nbCoords + "' " + "value='44.79517'>" +
-    "<label for='longitude'>Longitude</label>" + "<input type='text' name='longitude' id='longitude" + nbCoords + "' " + "value='-0.603537'>"
+    newLine.innerHTML = "<label for='latitude" + nbCoords + "'>Latitude</label>" + "<input type='text' name='latitude" + nbCoords + "' id='latitude" + nbCoords + "' " + "value='44.79517'>" +
+    "<label for='longitude" + nbCoords + "'>Longitude</label>" + "<input type='text' name='longitude" + nbCoords + "' id='longitude" + nbCoords + "' " + "value='-0.603537'>"
     + '<input type="button" value="-" class="remove-line-button" onclick="removeCoords(this)">';
     console.log(newLine.innerHTML);
     formLines.appendChild(newLine);
