f222446bc834013ddb1c202b6bfc312d79a1f9bf
Louis-Victor Ladagnous <louivictor@live.fr>
2023-01-31 09:39:10 +0100

    Requetes gares_ter et lignes_car

--- sources/requetes/gares_ter.php
+++ sources/requetes/gares_ter.php
@@ -2,3 +2,29 @@
     On fait la liste des gares_ter, avec pour chacun toutes ses informations statiques mais aussi : 
 - la position GPS pas besoin d'en faire une liste car cela sera un point unique
 -->
+
+<?php
+$db = new mysqli("localhost", "root", "@Password0", "campus");
+
+// On execute notre requete SQL
+if ($result = $db->query("
+SELECT gares_ter.*, latitude, longitude
+FROM gares_ter
+INNER JOIN siter_gares_ter
+    ON gares_ter.id = siter_gares_ter.gare_ter
+INNER JOIN coordonnees_gares_ter
+    ON siter_gares_ter.coordonnee = coordonnees_gares_ter.id
+;
+")) {
+    while ($row = $result->fetch_assoc()) {
+        $parkings[] = $row;
+    }
+    // on libere la memoire
+    $result->free();
+}
+// on encode la liste des stations au format json, et on l'affiche
+echo json_encode($parkings);
+
+$db->close();
+
+?>
--- sources/requetes/lignes_cars.php
+++ sources/requetes/lignes_cars.php
@@ -5,3 +5,49 @@
 - celui de destination
 - pas besoin de la liste des arrets !
 -->
+
+<?php
+$db = new mysqli("localhost", "root", "@Password0", "campus");
+
+// On execute notre requete SQL
+if ($result = $db->query("
+SELECT lignes_cars.*, latitude, longitude
+FROM lignes_cars
+INNER JOIN situer_lignes_cars
+    ON lignes_cars.id = situer_lignes_cars.ligne_car
+INNER JOIN coordonnees_lignes_cars
+    ON situer_lignes_cars.coordonnee = coordonnees_lignes_cars.id
+;
+")) {
+    while ($row = $result->fetch_assoc()) {
+        // une particularité de cet exemple : chaque station de velo peut avoir plusieur types d'accroches
+        // cette exemple peut donc servir de base pour les autres requetes qui ont des relations 1-n ou n-n (plusieurs types d'accroches, plusieurs types de vehicules autorisés, plusieurs points gps (cf lignes), etc.)
+        
+        // on ajoute une colone "type_accroche" à notre tableau $row afin de stocker les types d'accroches de la station correspondant à la ligne courante
+        $row = array_merge($row, array("arret_cars_depart" => array()));
+        $row = array_merge($row, array("arret_cars_destination" => array()));
+        // on recupere les types d'accroches de la station courante
+        $result2 = $db->query("SELECT arrets_cars.nom as arret_cars_depart FROM arrets_cars INNER JOIN lignes_cars ON arrets_cars.id = lignes_cars.depart WHERE arrets_cars.id = ".$row['depart'].";");
+        $result3 = $db->query("SELECT arrets_cars.nom as arret_cars_destination FROM arrets_cars INNER JOIN lignes_cars ON arrets_cars.id = lignes_cars.destination WHERE arrets_cars.id = ".$row['destination'].";");
+
+        $row2 = $result2->fetch_assoc();
+        $row3 = $result3->fetch_assoc();
+
+        $row['arret_cars_depart'][] = $row2['arret_cars_depart'];
+        $row['arret_cars_destination'][] = $row3['arret_cars_destination'];
+
+        // on libere la memoire
+        $result2->free();
+        $result3->free();
+        // on ajoute la station courante à la liste des stations
+        $parkings[] = $row;
+    }
+    // on libere la memoire
+    $result->free();
+}
+// on encode la liste des stations au format json, et on l'affiche
+echo json_encode($parkings);
+
+$db->close();
+
+?>
