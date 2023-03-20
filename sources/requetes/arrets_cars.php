a3e7e237d30d7120df9d5e8a83528e4ab030be50
Louis-Victor Ladagnous <louivictor@live.fr>
2023-03-20 21:21:15 +0100

    Bug réglé sur les arrets_cars

--- sources/requetes/arrets_cars.php
+++ sources/requetes/arrets_cars.php
@@ -15,16 +15,16 @@ INNER JOIN coordonnees_arrets_cars
         // une particularité de cet exemple : chaque station de velo peut avoir plusieur types d'accroches
         // cette exemple peut donc servir de base pour les autres requetes qui ont des relations 1-n ou n-n (plusieurs types d'accroches, plusieurs types de vehicules autorisés, plusieurs points gps (cf lignes), etc.)
         
-        // on ajoute une colone "type_accroche" à notre tableau $row afin de stocker les types d'accroches de la station correspondant à la ligne courante
+        // on ajoute une colone "ligne_car" à notre tableau $row afin de stocker les types d'accroches de la station correspondant à la ligne courante
         $row = array_merge($row, array("ligne_car" => array()));
         // on recupere les types d'accroches de la station courante
-        $result2 = $db->query("SELECT lignes_cars.nom FROM desservir_car INNER JOIN lignes_cars ON desservir_car.ligne_car = lignes_cars.id WHERE arret_car = ".$row['id'].";");
+        $result2 = $db->query("SELECT lignes_cars.numero FROM desservir_car INNER JOIN lignes_cars ON desservir_car.ligne_car = lignes_cars.id WHERE arret_car = ".$row['id'].";");
         
         // on parcourt les types d'accroches de la station courante
-        while ($row2 = $result2->fetch_assoc()) {
-            // on ajoute le type d'accroche courant à la liste des types d'accroches de la station courante
-            $row['ligne_car'][] = $row2['ligne_car'];
-        }
+        // while ($row2 = $result2->fetch_assoc()) {
+        //     // on ajoute le type d'accroche courant à la liste des types d'accroches de la station courante
+        //     $row['ligne_car'][] = $row2['numero'];
+        // }
         // on libere la memoire
         $result2->free();
         // on ajoute la station courante à la liste des stations
