c01d27462988ad89b88a74d6a2b49c70fd2c9cbc
THIVIN Nathan <Nathan.Thivin@bordeaux-inp.fr>
2023-03-29 19:00:03 +0200

    corrections charge et covoit

--- sources/map/BDD/points_charges_bdd.js
+++ sources/map/BDD/points_charges_bdd.js
@@ -4,21 +4,23 @@
 // DEJA DEFINI DANS API/points_charges.js
 
 // PARTIE DE CODE A MODIFIER ==>
+var pdcIcon = L.icon({
+    iconUrl: './sources/icons/pdc.png',
+
+    iconSize:     [17, 17], // size of the icon
+});
 
 
 function afficherPopupPointsDeCharges(e) { 
     var nom = "Nom : " + e.nom;
     var infos = "Informations : "+ e.info_complementaires;
-    // CECI EST A MOFIER POUR AFFICHER LES INFORMATIONS DES POINTS DE CHARGES
 
-    // EXEMPLE : PUISSANCE DES PRISES, TYPE DE PRISE, ETC...
-
-    // var vehicules_autorises = [];
-    // for (var i = 0; i < e.vehicules.length; i++) {
-    //     vehicules.push(e.vehicules[i]);
-    // }
-    // var vehicules = "Vehicules autorisés : " + vehicules.join(" - ");
-    // var popup = nom + "<br>" + infos + "<br>" + vehicules;
+    var prises = []
+    for (var i = 0; i < e.type_prise.length; i++) {
+        prises.push("Type : " + e.type_prise[i] + " -- Puissance : " + e.puissance[i] + " kW <br>");
+    }
+  
+    var popup = nom + "<br>" + infos + "<br>" + prises;
     return popup;
 }
 
@@ -34,28 +36,16 @@ function updateBddStationsPointsDeCharges() { //a renommer en updateBddPointsCha
     fetch('./sources/requetes/points_de_charges.php')
     .then(response => response.json())
     .then(data => {
-        data.forEach(function(pointsDeCharge) {//pour chaque point de charge
-            if (pointsDeCharge.coordonnees.length == 1) // si le point de charge n'a qu'une seule coordonnée
-            { // on ajoute un marker
-                var marker = L.marker([pointsDeCharge.coordonnees[0].latitude, pointsDeCharge.coordonnees[0].longitude], {icon : pdcIcon}).addTo(feature_group_pdc);
-                marker.bindPopup(afficherPopupPointsDeCharge(pointsDeCharge)); // on ajoute une popup
-                marker.setStyle({color: 'grey'}); // on definie la couleur du marker
-            }
-            else if (pointsDeCharge.coordonnees.length > 1) // si le point de charge a plusieurs coordonnées
-            {
-                var latlngs = []; // on crée un tableau de coordonnées
-                pointsDeCharge.coordonnees.forEach(function(coordonnee) { // pour chaque coordonnée
-                    latlngs.push([coordonnee.latitude,coordonnee.longitude]); // on ajoute la coordonnée au tableau
-                }
-                );
-                var polygon = L.polygon(latlngs).addTo(feature_group_pdc); // on ajoute un polygone sur la carte avec les coordonnées du tableau
-                polygon.setStyle({color: 'grey'}); // on definie la couleur du polygone
-                polygon.bindPopup(afficherPopupPointsDeCharge(pointsDeCharge)); // on ajoute une popup
-            }
+        data.forEach(function(pdc) { // pour chaque pdc
+    
+            var marker = L.marker([pdc.latitude, pdc.longitude],  {icon : pdcIcon}).addTo(feature_group_pdc); // création du marqueur
+            marker.bindPopup(afficherPopupPointsDeCharges(pdc)); // ajout du popup
+            //marker.setStyle({color: 'yellow'}); // on definie la couleur du marker
+
         });
         
     })
     .catch(error => console.error(error));
 }
 
-// updateBddStationsPointsDeCharges();
+updateBddStationsPointsDeCharges();
