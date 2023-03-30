702678fbf8d07153edd96fa6f7430e8cc5e9b6db
BELKHEDRA Yamine <Yamine.Belkhedra@bordeaux-inp.fr>
2023-03-30 22:30:01 +0200

    fichier ajoute au mauvais endroit

--- sources/requetes/API_flutter/stations_velo_add.php
+++ sources/requetes/API_flutter/stations_velo_add.php
@@ -1,54 +1,49 @@
 <?php
 
-// on recupere les infos du formulaire
+// on recupere le gid du parking à supprimer par get
 $nom = $_GET['nom'];
 $nb_places = $_GET['nb_places'];
-$securise = $_GET['securise'];
-$abrite = $_GET['abrite'];
-$type_accroche = $_GET['type_accroche'];
+$type_accroche = json_decode($_GET['type_accroche']);
 $info_complementaires = $_GET['info_complementaires'];
 $latitude = $_GET['latitude'];
 $longitude = $_GET['longitude'];
+$securise = $_GET['securise'];
+$abrite = $_GET['abrite'];
 
 $db = new mysqli("localhost", "root", "@Password0", "campus");
 
-$sql = "INSERT INTO stations_velo (nom, nb_places, securise, abrite, info_complementaires) VALUES ('$nom', '$nb_places', '$securise', '$abrite', $info_complementaires')";
+//ajouter les données dans la table parking
+$sql = "INSERT INTO stations_velo (nom, nb_places, securise, abrite, info_complementaires) VALUES ('$nom', '$nb_places', '$securise', '$abrite', '$info_complementaires')";
 echo $sql;
 //exécution de la requête
 if ($db->query($sql)) {
-    $sql2 = "INSERT INTO coordonnees_stations_velo (latitude, longitude) VALUES ('$latitude', '$longitude')";
-    echo $sql2;
     $id_stations_velo_ajouter = $db->insert_id;
-
-    if ($db->query($sql2)) {
-
+    $sql = "INSERT INTO coordonnees_stations_velo (latitude, longitude) VALUES ('$latitude', '$longitude')";
+    echo $sql;
+    if ($db->query($sql)) {
         $id_coordonnees_stations_velo_ajouter = $db->insert_id;
-        
-        // ajouter les données dans la table situer_parkings (id_parking, id_coordonnees_parking)
-        $sql3 = "INSERT INTO situer_stations_velo (pt_velo, coordonnee) VALUES ('$id_stations_velo_ajouter', '$id_coordonnees_stations_velo_ajouter')";
-        echo $sql3;
-        if ($db->query($sql3)) {
-            $sql4 = "INSERT INTO installer (station_velo, type_accroche) VALUES ('$id_stations_velo_ajouter', '$type_accroche')";
-            echo $sql4;
-            if ($db->query($sql4)) {
-                echo "Station de vélo ajouté";
+        $sql = "INSERT INTO situer_stations_velo (pt_velo, coordonnee) VALUES ('$id_stations_velo_ajouter', '$id_coordonnees_stations_velo_ajouter')";
+        echo $sql;
+        if($db->query($sql)) {
+            for ($i = 0; $i < count($type_accroche); $i++) {
+                $accroche = $type_accroche[$i];
+                $sql = "INSERT INTO installer (station_velo, type_accroche) VALUES ('$id_stations_velo_ajouter', '$accroche')";
+                echo $sql;
+                if ($db->query($sql)) {
+                    $success = 1;
+                } else {
+                    $success = 0;
+                    echo "Erreur : " . $db->error;
+                }
             }
-            else {
-                $success = 0;
-                echo "Erreur : " . $db->error;
-            }
-        }
-        else {
+        } else {
             $success = 0;
             echo "Erreur : " . $db->error;
         }
-
-    }
-    else {
+    } else {
         $success = 0;
         echo "Erreur : " . $db->error;
     }
-
 }
 else {
     $success = 0;
