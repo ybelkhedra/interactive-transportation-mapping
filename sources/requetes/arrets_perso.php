f4aec5f8d82be8a031f725fc624fa6085f61fa05
THIVIN NATHAN <nthivin@bordeaux-inp.fr>
2023-05-12 10:31:19 +0200

    menage encore

--- sources/requetes/arrets_perso.php
+++ sources/requetes/arrets_perso.php
@@ -1,17 +1,5 @@
 <?php
-// <!-- FICHIER DE REQUETE POUR LES ARRETS -->
 
-
-// <!-- 
-//     On fait la liste des arrets, avec pour chacun toutes ses informations statiques mais aussi : 
-// - la position GPS pas besoin d'en faire une liste car cela sera un point unique
-// - la station vcube a proximite
-// - la liste des lignes qui lui sont associées
-// - heure de premier passage de chaque ligne qui lui sont associées
-// - heure de dernier passage de chaque ligne qui lui sont associées
-// - frequence de chaque ligne qui lui sont associées
-// - diurne/nocturne de chaque ligne qui lui sont associées
-// -->
 
 $db = new mysqli("localhost", "root", "@Password0", "campus");
 
