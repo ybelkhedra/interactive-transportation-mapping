720c7eb32dcd03adcd570fdad812417a273f0091
REKHIS Rafik <rrekhis@enseirb-matmeca.fr>
2023-06-30 10:04:17 +0200

    trafic par jour

--- sources/map/BDD/trafic_jour_bdd.js
+++ sources/map/BDD/trafic_jour_bdd.js
@@ -7,9 +7,22 @@ function afficherPopupTraficJour(capteur)
 {
     var nom = "Nom : " + capteur.nom;
     var type = "Matériel Utilisé : " + capteur.type_capteur;
-    // var e_s = "Entrée/Sortie total : " + capteur.entree_sortie;
     
-    var popup = nom + "<br>" + type + "<br> Trafic par jour";
+    var popup = nom + "<br>" + type + "<br> Trafic par jour: " + document.getElementById("datepicker").value;
+    popup += "<br>Total Véhicules : " + capteur.total_vehicules;
+    popup += "<br>EDPM/Trottinette : " + capteur.total_vehicules_EDPM_Trottinette;
+    popup += "<br>VELO : " + capteur.total_vehicules_VELO;
+    popup += "<br>MOTO : " + capteur.total_vehicules_MOTO;
+    popup += "<br>Deux roues/2RM/2R : " + capteur.total_vehicules_Deux_roues_2RM_2R;
+    popup += "<br>VL : " + capteur.total_vehicules_VL;
+    popup += "<br>BUS : " + capteur.total_vehicules_BUS;
+    popup += "<br>PL : " + capteur.total_vehicules_PL;
+    popup += "<br>PL_1 : " + capteur.total_vehicules_PL_1;
+    popup += "<br>PL_2 : " + capteur.total_vehicules_PL_2;
+    popup += "<br>PL/BUS : " + capteur.total_vehicules_PL_BUS;
+    popup += "<br>UT : " + capteur.total_vehicules_UT;
+    popup += "<br>PT : " + capteur.total_vehicules_PT;
+
     return popup;
 }
 
@@ -22,7 +35,7 @@ function updateBddTraficJour(){
 
     // suppression des marqueurs existants de la carte
     feature_group_trafic_jour_bdd.eachLayer(function (layer) {
-        if (layer instanceof L.Marker) {
+        if (layer instanceof L.Marker || layer instanceof L.Circle) {
             feature_group_trafic_jour_bdd.removeLayer(layer);
         }
         });
@@ -45,30 +58,30 @@ function updateBddTraficJour(){
             var marker = L.marker([capteur.latitude, capteur.longitude], { icon: L.icon({ iconUrl: iconUrl, iconSize: [taille_icon, taille_icon] }) }).addTo(feature_group_trafic_jour_bdd);
             marker.bindPopup(afficherPopupTraficJour(capteur));
             
-            // if (capteur.entree_sortie > 0) {
-            //     var circleColor;
-            //     var circleRadius = 40 + Math.floor(capteur.entree_sortie / 200);
+            if (capteur.total_vehicules > 0) {
+                var circleColor;
+                var circleRadius = 40 + Math.floor(capteur.total_vehicules / 200);
                 
-            //     if (capteur.entree_sortie >= 15000) {
-            //         circleColor = "darkred";
-            //     } else if (capteur.entree_sortie >= 10000) {
-            //         circleColor = "red";
-            //     } else if (capteur.entree_sortie >= 5000) {
-            //         circleColor = "orange";
-            //     } else if (capteur.entree_sortie >= 2500) {
-            //         circleColor = "yellow";
-            //     } else {
-            //         circleColor = "green";
-            //     }
+                if (capteur.total_vehicules >= 15000) {
+                    circleColor = "darkred";
+                } else if (capteur.total_vehicules >= 10000) {
+                    circleColor = "red";
+                } else if (capteur.total_vehicules >= 5000) {
+                    circleColor = "orange";
+                } else if (capteur.total_vehicules >= 2500) {
+                    circleColor = "yellow";
+                } else {
+                    circleColor = "green";
+                }
                 
-            //     L.circle([capteur.latitude, capteur.longitude], {
-            //         color: circleColor,
-            //         fillColor: circleColor,
-            //         fillOpacity: 0.5,
-            //         radius: circleRadius,
-            //         stroke: false
-            //     }).addTo(feature_group_trafic_jour_bdd);
-            // }
+                L.circle([capteur.latitude, capteur.longitude], {
+                    color: circleColor,
+                    fillColor: circleColor,
+                    fillOpacity: 0.5,
+                    radius: circleRadius,
+                    stroke: false
+                }).addTo(feature_group_trafic_jour_bdd);
+            }
             
             affichage();
         });                
--- sources/requetes/trafic_jour.php
+++ sources/requetes/trafic_jour.php
@@ -1,22 +1,52 @@
 <?php
 $db = new mysqli("localhost", "root", "@Password0", "campus");
 
-// On execute notre requete SQL
-if ($result = $db->query("
-SELECT *
-FROM capteurs
-;
-")) {
-    $capteurs = array();
-    while ($row = $result->fetch_assoc()) {
-        $capteurs[] = $row;
+if(isset($_GET['date'])) {
+    $date = $_GET['date'];
+
+    // Requête SQL pour sélectionner les informations des capteurs et le nombre total de véhicules par classe pour la date donnée
+    $sql = "SELECT
+                c.*,
+                COUNT(d.classe_vehicule) AS total_vehicules,
+                COUNT(CASE WHEN cv.classe = 'EDPM/Trottinette' THEN d.classe_vehicule END) AS total_vehicules_EDPM_Trottinette,
+                COUNT(CASE WHEN cv.classe = 'VELO' THEN d.classe_vehicule END) AS total_vehicules_VELO,
+                COUNT(CASE WHEN cv.classe = 'MOTO' THEN d.classe_vehicule END) AS total_vehicules_MOTO,
+                COUNT(CASE WHEN cv.classe = 'Deux roues/2RM/2R' THEN d.classe_vehicule END) AS total_vehicules_Deux_roues_2RM_2R,
+                COUNT(CASE WHEN cv.classe = 'VL' THEN d.classe_vehicule END) AS total_vehicules_VL,
+                COUNT(CASE WHEN cv.classe = 'BUS' THEN d.classe_vehicule END) AS total_vehicules_BUS,
+                COUNT(CASE WHEN cv.classe = 'PL' THEN d.classe_vehicule END) AS total_vehicules_PL,
+                COUNT(CASE WHEN cv.classe = 'PL_1' THEN d.classe_vehicule END) AS total_vehicules_PL_1,
+                COUNT(CASE WHEN cv.classe = 'PL_2' THEN d.classe_vehicule END) AS total_vehicules_PL_2,
+                COUNT(CASE WHEN cv.classe = 'PL/BUS' THEN d.classe_vehicule END) AS total_vehicules_PL_BUS,
+                COUNT(CASE WHEN cv.classe = 'UT' THEN d.classe_vehicule END) AS total_vehicules_UT,
+                COUNT(CASE WHEN cv.classe = 'PT' THEN d.classe_vehicule END) AS total_vehicules_PT
+            FROM capteurs c
+            LEFT JOIN donnees_capteurs d ON c.id = d.capteur
+            LEFT JOIN classes_vehicules cv ON d.classe_vehicule = cv.id
+            WHERE DATE(d.horodate) = '$date'
+            GROUP BY c.id, c.nom, c.type_capteur";
+
+    // Exécution de la requête
+    $result = $db->query($sql);
+
+    // Vérifier s'il y a des résultats
+    if ($result && $result->num_rows > 0) {
+        $capteurs = array();
+        while ($row = $result->fetch_assoc()) {
+            $capteurs[] = $row;
+        }
+    } else {
+        echo "Aucun résultat trouvé pour la date donnée.";
     }
-    // on libere la memoire
+
+    // Libérer la mémoire du résultat
     $result->free();
+} else {
+    echo "La date n'est pas spécifiée dans l'URL.";
 }
+
 // on encode la liste des stations au format json, et on l'affiche
 echo json_encode($capteurs);
 
 $db->close();
-
 ?>
